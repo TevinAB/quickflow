3dfb78799595aa7be121b9de19b3de577f2e4774
Tevin Banton <tevinanthony95@gmail.com>
2021-09-17 18:24:38 -0500

    Refactored to remove duplicate code

--- src/server/note/routes.ts
+++ src/server/note/routes.ts
@@ -1,17 +1,23 @@
 import { Router } from 'express';
 import NoteModel from './model';
 import {
-  getNoteWrapper,
-  createNoteWrapper,
-  updateNoteWrapper,
-  deleteNoteWrapper,
-} from './controllers';
+  wrapperGetResource,
+  wrapperCreateResource,
+  wrapperUpdateResource,
+  wrapperDeleteResource,
+} from '../utils/resourceMethods';
 
 const noteRoutes = Router();
 
-noteRoutes.get('/', getNoteWrapper(NoteModel));
-noteRoutes.post('/create', createNoteWrapper(NoteModel));
-noteRoutes.put('/update', updateNoteWrapper(NoteModel));
-noteRoutes.delete('/delete', deleteNoteWrapper(NoteModel));
+noteRoutes.get('/', wrapperGetResource(NoteModel, 'Note not found.'));
+noteRoutes.post('/create', wrapperCreateResource(NoteModel, 'Note not added.'));
+noteRoutes.put(
+  '/update',
+  wrapperUpdateResource(NoteModel, 'Note update failed.')
+);
+noteRoutes.delete(
+  '/delete',
+  wrapperDeleteResource(NoteModel, 'Note delete failed.', 'Note deleted.')
+);
 
 export default noteRoutes;
