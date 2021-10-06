db04dbe7f829209bcd0df3b764cbbc37b8f71dc9
Tevin Banton <tevinanthony95@gmail.com>
2021-10-06 17:49:55 -0500

    added document request functions

--- src/client/src/services/document/deleteDoc.ts
+++ src/client/src/services/document/deleteDoc.ts
@@ -1,12 +1,83 @@
 import { getFromLocalStorage } from '../localStorage';
-import { _delete } from '../requests';
+import { get, post, put, _delete } from '../requests';
+
+export async function getDocumentOne(
+  docType: string,
+  id: string,
+  token: string | undefined
+) {
+  const _token = defaultToken(token);
+
+  const result = await get(
+    `/api/resource/document/${docType.toLowerCase()}/${id}`,
+    {
+      'x-auth-token': _token,
+    }
+  );
+
+  return result;
+}
+
+export async function getDocumentMany(
+  docType: string,
+  token: string,
+  pageNum: number,
+  itemsPerPage = 15
+) {
+  const _token = defaultToken(token);
+  const result = await get(
+    `/api/resource/document/${docType.toLowerCase()}?pageNum=${pageNum}&itemsPerPage=${itemsPerPage}`,
+    { 'x-auth-token': _token }
+  );
+
+  return result;
+}
+
+export async function createDocument(
+  docType: string,
+  data: {},
+  token: string | undefined
+) {
+  const _token = defaultToken(token);
+
+  const result = await post(
+    `/api/resource/document/${docType.toLowerCase()}`,
+    {
+      'x-auth-token': _token,
+      'content-type': 'application/json',
+    },
+    data
+  );
+
+  return result;
+}
+
+export async function editDocument(
+  docType: string,
+  id: string,
+  data: {},
+  token: string | undefined
+) {
+  const _token = defaultToken(token);
+
+  const result = await put(
+    `/api/resource/document/${docType.toLowerCase()}/${id}`,
+    {
+      'x-auth-token': _token,
+      'content-type': 'application/json',
+    },
+    data
+  );
+
+  return result;
+}
 
 export async function deleteDocument(
   docType: string,
   id: string | Array<string>,
   token: string | undefined
 ) {
-  const _token = token ?? getFromLocalStorage('token');
+  const _token = defaultToken(token);
   let _id;
 
   if (Array.isArray(id)) {
@@ -16,10 +87,14 @@ export async function deleteDocument(
   }
 
   const result = await _delete(
-    `/api/resource/document/${docType}`,
-    { 'x-auth-token': _token },
+    `/api/resource/document/${docType.toLowerCase()}`,
+    { 'x-auth-token': _token, 'content-type': 'application/json' },
     { _ids: _id }
   );
 
   return result;
+}
+
+function defaultToken(token: string | undefined) {
+  return token ?? getFromLocalStorage('token');
 }
