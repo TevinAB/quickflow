a8f62ef0cd9ba64617e37d1591a53e68eaff6f58
Tevin Banton <tevinanthony95@gmail.com>
2021-10-07 16:06:20 -0500

    form data request functions

--- src/client/src/mock-sw/formData/index.ts
+++ src/client/src/mock-sw/formData/index.ts
@@ -1,19 +1,44 @@
 import getFormData from './responses/getFormData.json';
 import editFormData from './responses/editFormData.json';
 import listData from './responses/getListsData.json';
+import { validateRequest } from '../utils';
 import { rest } from 'msw';
 
 export const formDataHanlders = [
   rest.get(
-    '/api/resource/customization/form/contact',
+    '/api/resource/customization/form/:type',
     (request, response, ctx) => {
-      return response(ctx.status(200), ctx.json(getFormData));
+      const valid = validateRequest(request, {
+        headers: ['x-auth-token'],
+        pathParams: ['type'],
+      });
+
+      if (valid) {
+        return response(ctx.status(200), ctx.json(getFormData));
+      } else {
+        return response(ctx.status(400));
+      }
     }
   ),
-  rest.get('/api/resource/customization/lists', (request, response, ctx) => {
-    return response(ctx.status(200), ctx.json(listData));
+  rest.put('/api/resource/customization/form/:id', (request, response, ctx) => {
+    const valid = validateRequest(request, {
+      headers: ['x-auth-token', 'content-type'],
+      pathParams: ['id'],
+    });
+
+    if (valid) {
+      return response(ctx.status(200), ctx.json(editFormData));
+    } else {
+      return response(ctx.status(400));
+    }
   }),
-  rest.put('/api/resource/customization/form', (request, response, ctx) => {
-    return response(ctx.status(200), ctx.json(editFormData));
+  rest.get('/api/resource/customization/lists', (request, response, ctx) => {
+    const valid = validateRequest(request, { headers: ['x-auth-token'] });
+
+    if (valid) {
+      return response(ctx.status(200), ctx.json(listData));
+    } else {
+      return response(ctx.status(400));
+    }
   }),
 ];
