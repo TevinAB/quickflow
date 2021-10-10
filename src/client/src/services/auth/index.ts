75bf0d857b33fd7305c202a36f1fb5eb0052d94d
Tevin Banton <tevinanthony95@gmail.com>
2021-10-10 12:55:17 -0500

    new types added.

--- src/client/src/services/auth/index.ts
+++ src/client/src/services/auth/index.ts
@@ -1,11 +1,12 @@
 import { post } from '../requests';
+import { LoginData, SignUpData } from '../../types';
 import { buildHeaders } from '../headers';
 
-export async function login(data: {}) {
+export async function login(data: LoginData) {
   return await auth('/api/auth/login', data);
 }
 
-export async function signUp(data: {}) {
+export async function signUp(data: SignUpData) {
   return await auth('/api/auth/sign_up', data);
 }
 
