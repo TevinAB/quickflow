392f6c926edfc82a6704180768dda8d2965b1b61
Tevin Banton <tevinanthony95@gmail.com>
2021-10-12 11:04:51 -0500

    widget footer update

--- src/client/src/components/WidgetFooter/index.test.tsx
+++ src/client/src/components/WidgetFooter/index.test.tsx
@@ -1,13 +1,24 @@
 import WidgetFooter from '.';
+import { ReactNode } from 'react';
 import { render } from '@testing-library/react';
+import { ThemeProvider } from '@mui/material';
+import { theme } from '../../theme';
 
 describe('Widget Footer test', () => {
+  const ThemeWrapper = ({ children }: { children: ReactNode }) => {
+    return (
+      <ThemeProvider theme={theme}>
+        <WidgetFooter>{children}</WidgetFooter>
+      </ThemeProvider>
+    );
+  };
+
   it('should render', () => {
     const text = 'View all.';
     const { getByText } = render(
-      <WidgetFooter>
+      <ThemeWrapper>
         <div>{text}</div>
-      </WidgetFooter>
+      </ThemeWrapper>
     );
 
     getByText(text);
--- src/client/src/components/WidgetFooter/index.tsx
+++ src/client/src/components/WidgetFooter/index.tsx
@@ -1,17 +1,26 @@
 import { ReactNode } from 'react';
+import { useTheme } from '@mui/material';
+import { SxProps } from '@mui/system';
 import Box from '../Box';
 
 type WidgetFooterProps = {
   children: ReactNode;
 };
 
-const baseStyle = {
-  backgroundColor: 'secondary.main',
-  display: 'flex',
-  justifyContent: 'center',
-  padding: 1,
-} as const;
-
 export default function WidgetFooter({ children }: WidgetFooterProps) {
+  const theme = useTheme();
+
+  const baseStyle: SxProps = {
+    backgroundColor: 'secondary.main',
+    display: 'flex',
+    borderStyle: 'solid',
+    borderWidth: theme.widgets.borderSize,
+    borderColor: theme.widgets.borderColor,
+    borderBottomLeftRadius: theme.widgets.borderRadius,
+    borderBottomRightRadius: theme.widgets.borderRadius,
+    justifyContent: 'center',
+    padding: 1,
+  };
+
   return <Box sx={baseStyle}>{children}</Box>;
 }
