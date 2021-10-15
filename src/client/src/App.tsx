664725934840975e0ee61ecf7e844017de5290d6
Tevin Banton <tevinanthony95@gmail.com>
2021-10-14 19:15:12 -0500

    misc changes

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
 import './App.css';
+import { Provider } from 'react-redux';
+import store from './store';
+import { ThemeProvider } from '@mui/material';
+import { theme } from './theme';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import SideBar from './components/SideBar';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div
+      style={{
+        paddingTop: 0,
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'start',
+      }}
+      className="App"
+    >
+      <Router>
+        <ThemeProvider theme={theme}>
+          <Provider store={store}>
+            <SideBar renderLogo={true} renderCollapseButton={true} />
+          </Provider>
+        </ThemeProvider>
+      </Router>
     </div>
   );
 }
