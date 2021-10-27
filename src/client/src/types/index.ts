d74f5be3c5458955a51aa9a5f0dba9227cf43a80
Tevin Banton <tevinanthony95@gmail.com>
2021-10-27 18:50:37 -0500

    type and util updates

--- src/client/src/types/index.ts
+++ src/client/src/types/index.ts
@@ -4,9 +4,39 @@ import { groupBy } from '../utils';
 
 export type Cancel_Token = CancelToken;
 export type Cancel_Token_Source = CancelTokenSource;
-export type RequestError = AxiosError;
+export type RequestError = AxiosError<ApiError>;
+
+export type BaseDocument = {
+  _id: string;
+  __type: DocumentType;
+  name: string;
+  timeline_id: string;
+  owner: { text: string; value: string };
+};
+
+export type Contact = {
+  first_name: string;
+  last_name: string;
+} & BaseDocument;
+
+export type Account = {
+  associated_contacts?: Array<{ first_name: string; last_name: string }>;
+};
+
+//profile type
 
 export type DocumentType = 'Contact' | 'Account' | 'Deal';
+export type DocumentStoreState<T extends BaseDocument> = {
+  documentData: T;
+  timelineData: TimelineData;
+  isLoading: boolean;
+  documentLoadError: boolean;
+  errorMessage?: string;
+};
+export type DocumentThunkArgs = {
+  documentId: string;
+  token: string;
+};
 
 export type DealRangeTypes = 'Year';
 export type DealStatus = 'Open' | 'Won' | 'Lost';
@@ -16,9 +46,11 @@ export type Deal = {
   value: number;
   timeline_id: string;
   deal_status: DealStatus;
+  pipeline: string;
   created_date: string;
   closed_date: string;
-} & SearchResultItem;
+} & BaseDocument &
+  Pick<Account, 'associated_contacts'>;
 
 export type GroupedDeals = Array<{
   categoryTitle: string;
@@ -74,12 +106,6 @@ export type FormFieldData = {
   _id: string;
 };
 
-//contact type
-
-//account type
-
-//profile type
-
 export type ActivityType = 'Task' | 'Event';
 
 export type ActivityBase = {
@@ -96,6 +122,9 @@ export type Event = SearchResultItem & ActivityBase;
 
 export type SearchType = 'Profile' | DocumentType;
 
+export type TimelineItem = typeof timeline.timeline_items[0];
+export type TimelineData = typeof timeline;
+
 //A work around to take the type of a generic function.
 function wrapperGeneric() {
   const obj = timeline.timeline_items;
@@ -104,7 +133,7 @@ function wrapperGeneric() {
     return 'Doesnt actually need to return anything here.';
   });
 }
-export type TimelineData = ReturnType<typeof wrapperGeneric>;
+export type TimelineFormattedData = ReturnType<typeof wrapperGeneric>;
 
 export type TimelineItemType = 'Note' | 'Other' | ActivityType;
 
