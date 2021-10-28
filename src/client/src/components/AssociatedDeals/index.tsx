ec39ca4328ea0a0b642df2136adc0d336d8d06eb
Tevin Banton <tevinanthony95@gmail.com>
2021-10-27 19:01:35 -0500

    associated deal component update

--- src/client/src/components/AssociatedDeals/index.tsx
+++ src/client/src/components/AssociatedDeals/index.tsx
@@ -3,6 +3,9 @@ import { numberFormat } from '../../utils';
 import { Link } from 'react-router-dom';
 import { CURRENCY_DECIMAL } from '../../constants';
 import { Typography } from '@mui/material';
+import { useGetAssociatedDeals } from '../../hooks/deals';
+import { WidgetLoading, WidgetLoadError } from '../WidgetUtils';
+import { useAppSelector } from '../../hooks/redux';
 
 interface DealData {
   name: string;
@@ -11,17 +14,32 @@ interface DealData {
 }
 
 type AsscDealsProp = {
-  deals: Array<DealData>;
+  mainDocId: string;
 };
 
-export default function AssociatedDeals({ deals }: AsscDealsProp) {
-  return (
-    <div className="widget assc-deals">
-      <Typography fontWeight="bold" component="h2" className="widget__title">
-        Associated Deals ({deals.length})
-      </Typography>
+export default function AssociatedDeals({ mainDocId }: AsscDealsProp) {
+  const token = useAppSelector((state) => state.user.token);
+  const { assoicatedDeals, loading, error } = useGetAssociatedDeals(
+    mainDocId,
+    token
+  );
+
+  const renderError = (
+    <div>
+      <WidgetLoadError />
+    </div>
+  );
+
+  const renderLoading = (
+    <div>
+      <WidgetLoading />
+    </div>
+  );
+
+  const renderDeals = (
+    <div>
       <ul className="widget__body assc-deals__list">
-        {deals.map(({ _id, name, value }) => (
+        {assoicatedDeals.map(({ _id, name, value }) => (
           <li>
             <Link to={`/deal/${_id}`}>
               <Deal key={_id} _id={_id} name={name} value={value} />
@@ -29,6 +47,20 @@ export default function AssociatedDeals({ deals }: AsscDealsProp) {
           </li>
         ))}
       </ul>
+    </div>
+  );
+
+  const renderText = <p>No Associated Deals.</p>;
+
+  return (
+    <div className="widget assc-deals">
+      <Typography fontWeight="bold" component="h2" className="widget__title">
+        Associated Deals ({assoicatedDeals.length})
+      </Typography>
+      {error && !loading && renderError}
+      {!error && loading && renderLoading}
+      {!error && !loading && assoicatedDeals.length > 0 && renderDeals}
+      {!error && !loading && !assoicatedDeals.length && renderText}
     </div>
   );
 }
