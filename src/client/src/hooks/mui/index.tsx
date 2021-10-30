dff71c9b4f95bf74542bec102c8a377922bc75f2
Tevin Banton <tevinanthony95@gmail.com>
2021-10-30 10:54:21 -0500

    data grid update

--- src/client/src/hooks/mui/index.tsx
+++ src/client/src/hooks/mui/index.tsx
@@ -2,26 +2,44 @@ import type {
   FormData,
   MUITableColumns,
   ApiDocFormat,
+  RenderCellArgs,
 } from '../../types/index';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, ReactNode } from 'react';
 import { keyToLabel, sortAscending } from '../../utils';
+import { Link } from 'react-router-dom';
 
 export function useMuiTableColumns(rawColumnData: FormData) {
   const [columns, setColumns] = useState<Array<MUITableColumns>>([]);
 
   useEffect(() => {
     //form data stores info on what should be visible when viewing docs in table format
-    const { form_data } = rawColumnData;
+    const { form_data, res_type: documentType } = rawColumnData;
 
     const columnArray = form_data
       .slice()
       .sort((a, b) => sortAscending(a.list.order, b.list.order))
       .filter((fData) => fData.list.visible)
-      .map((fData) => ({
-        field: fData.field_name,
-        headerName: keyToLabel(fData.field_name),
-        width: 240,
-      }));
+      .map((fData) => {
+        let renderCell: (args: RenderCellArgs<ApiDocFormat>) => ReactNode;
+
+        //use links to display name/first_name in table
+        if (fData.field_name === 'name' || fData.field_name === 'first_name') {
+          renderCell = (args) => (
+            <Link to={`/${documentType}/${args.id}`}>{args.value}</Link>
+          );
+        } else {
+          renderCell = (args) => <span>{args.value}</span>;
+        }
+
+        const columnObject: MUITableColumns = {
+          field: fData.field_name,
+          headerName: keyToLabel(fData.field_name),
+          width: 240,
+          renderCell,
+        };
+
+        return columnObject;
+      });
 
     setColumns(columnArray);
   }, [rawColumnData]);
