397cda6b22e31e130af95f3e2d01c6f0d54cc0ac
Tevin Banton <tevinanthony95@gmail.com>
2021-10-31 18:07:05 -0500

    chore: small changes

--- src/server/createServer.ts
+++ src/server/createServer.ts
@@ -2,6 +2,7 @@ import express, { Request, Response, NextFunction } from 'express';
 import helmet from 'helmet';
 import apiRoutes from './api';
 import RequestException from './exceptions/requestException';
+import path from 'path';
 
 const server = express();
 
@@ -9,6 +10,11 @@ server.use(express.json());
 server.use(helmet());
 
 server.use('/api', apiRoutes);
+
+server.use(express.static(path.join(__dirname, '../client/build')));
+server.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
 
 server.use(function (
   error: RequestException,
